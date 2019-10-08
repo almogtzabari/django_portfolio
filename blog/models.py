f5387a25f2327725efe31b611a05e50d2509cae1
almogtzabari <almogtzabari@campus.technion.ac.il>
2019-10-08 18:33:12 +0300

    Jobs up and running in website

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    image = models.ImageField(upload_to='images/')
+    body = models.TextField(max_length=1000)
+
+
+
